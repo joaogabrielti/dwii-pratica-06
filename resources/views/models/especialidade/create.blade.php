205f182cb2e03000e2941e853e614c654b2ca7e1
João Gabriel Xavier <joaogabriel.informaticapgua@gmail.com>
2021-09-08 18:14:54 -0300

    iniciando implementação das interfaces de usuário

---
+++ resources/views/models/especialidade/create.blade.php
@@ -0,0 +1,1 @@
+@extends('layouts.app', ['page' => 'especialidade', 'title' => 'Cadastrar Nova Especialidade', 'icon' => 'clipboard'])
---
+++ resources/views/models/veterinario/create.blade.php
@@ -0,0 +1,1 @@
+@extends('layouts.app', ['page' => 'veterinario', 'title' => 'Cadastrar Novo Veterinário', 'icon' => 'file-earmark-person'])
